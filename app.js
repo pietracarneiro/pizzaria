480ab0a4e518b99d2fd478a77a9ac7d5bcfe7d28
Pietra Carneiro <pietrasc98@gmail.com>
2023-03-08 10:37:44 -0300

    Ajustando configurações de cookies e sessions

--- app.js
+++ app.js
@@ -4,18 +4,37 @@ const path = require('path');
 const bloqueiaForaDeHora = require('./middlewares/bloqueiaForaDeHora');
 const registraRequisicao = require('./middlewares/registraRequisicao');
 const router = require('./router');
+const session = require('express-session');
 
 // 2 - Criar o servidor ou aplicação -> basta executar a função express, a mesma retorna um servidor
 const servidor = express();
 servidor.set('view engine','ejs');
 
 // Define a pasta public como a pasta de arquivos estáticos
+servidor.use(session({
+    secret: 'SEGREDO',
+    resave: false,
+    saveUninitialized: true
+}))
 servidor.use(express.static(path.join(__dirname, 'public')))
 servidor.use(express.urlencoded({ extended: false }));
 
 // Configurando middlewares
 servidor.use(registraRequisicao)
 // servidor.use(bloqueiaForaDeHora)
+
+servidor.use(
+
+    (req, res, next) => {
+        if(req.session.admLogado){
+            console.log("Administrador logado....");
+        } else {
+            console.log("Visita qualquer... ");
+        }
+        next();
+    }
+
+)
 
 // 3 - Definir rota neste servidor
 // Endereço, método (get post put patch delete), função callback (a ação que o servidor vai realizar quando a req chegar)
--- router.js
+++ router.js
@@ -6,6 +6,7 @@ const AdmController = require('./controllers/AdmController');
 const PaginasController = require('./controllers/PaginasController');
 const PizzasController = require('./controllers/PizzasController');
 const registraRequisicao = require('./middlewares/registraRequisicao');
+const verificaSeLogado = require('./middlewares/verificaSeLogado');
 
 const fabricaDeMiddlewares = multer({dest:'public/img'})
 
@@ -25,13 +26,16 @@ router.get('/pizzas/:idDaPizza', PaginasController.showPizza);
 
 router.get('/api/pizzas', PizzasController.index);
 
-router.get('/adm/pizzas', AdmController.listarPizzas); // Mostrar lista das pizzas cadastradas
-router.get('/adm/pizzas/create', registraRequisicao, AdmController.criarPizza); // Mostrar form para add pizza
-router.post('/adm/pizzas/store', fabricaDeMiddlewares.single('img'), AdmController.gravarPizza); // Receber info digitadas para criação de um pizza
-router.get('/adm/pizzas/:id/edit', AdmController.showEditPizza); // Mostrar form para alterar pizza
-router.post('/adm/pizzas/update', ()=>{}); // Receber info digitadas para alteração de uma pizza
-router.post('/adm/pizzas/delete', ()=>{}); // Receber o id da pizza a ser removida
+router.get('/adm/login', AdmController.showLogin);
+router.post('/adm/login', AdmController.login);
 
+router.use('/adm', verificaSeLogado);
+router.get('/adm/pizzas', verificaSeLogado, AdmController.listarPizzas); // Mostrar lista das pizzas cadastradas
+router.get('/adm/pizzas/create', verificaSeLogado, registraRequisicao, AdmController.criarPizza); // Mostrar form para add pizza
+router.post('/adm/pizzas/store', verificaSeLogado, fabricaDeMiddlewares.single('img'), AdmController.gravarPizza); // Receber info digitadas para criação de um pizza
+router.get('/adm/pizzas/:id/edit', verificaSeLogado, AdmController.showEditPizza); // Mostrar form para alterar pizza
+router.post('/adm/pizzas/update', verificaSeLogado, ()=>{}); // Receber info digitadas para alteração de uma pizza
+router.post('/adm/pizzas/delete', verificaSeLogado, ()=>{}); // Receber o id da pizza a ser removida
 
 // Exportar o roteador
 module.exports = router;
